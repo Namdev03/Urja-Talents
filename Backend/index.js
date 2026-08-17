62f9f4efe434ad195f0d53bef27a4bd3238d4981
Namdev <namdevkadem6266@gmail.com>
2026-08-17 21:52:36 +0530

    Add initial package.json for backend setup with dependencies

---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import express from "express"
+const server = express();
+
+
+//=====Middelwares=====
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+server.get("/server",(req,res)=>{
+    res.send("hello Namdev")
+})
+const port = process.env.PORT;
+server.listen(port,()=>{
+    console.log(`server is live on port ${port}`);
+})
